6ab39834c328efe5048e694738bf6b4f1f4c6858
LoganKoch17 <kcr35@live.missouristate.edu>
2018-01-26 13:09:33 -0600

    new files added

---
+++ Proj2018-26-1.py
@@ -0,0 +1,15 @@
+from csc131 import dictionaries
+
+
+def main():
+    print("Main")
+
+def something_else():
+    d = (dictionaries.get_personal_data())
+    print(d)
+    for key in sorted(d.keys()):
+        print("%s: %s" % (key, d[key]))
+
+if __name__ == '__main__':
+    something_else()
+    main()
